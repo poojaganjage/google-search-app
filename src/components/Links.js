b0fba62a06402b2aea8617da20ad251aba319645
poojaganjage <pooja260895@gmail.com>
2022-11-30 20:02:55 +0530

    CSS File Added

--- src/components/Links.js
+++ src/components/Links.js
@@ -13,7 +13,10 @@ function Links() {
     <div className='flex sm:justify-around justify-between items-center mt-4'>
         {links.map(({url, text}, index) => {
             return (
-                <NavLink key={index} to={url} style={{color: 'blue', borderBottom: '2px', paddingBottom: '2px'}}>
+                // <NavLink key={index} to={url} style={{color: 'blue', borderBottom: '2px', paddingBottom: '2px'}}>
+                //     {text}
+                // </NavLink>
+                <NavLink key={index} to={url} activeClassName='text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2'>
                     {text}
                 </NavLink>
             );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import StateContextProvider from './context/StateContextProvider';
 import App from './App';
 import './global.css';
+import './dist/output.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
